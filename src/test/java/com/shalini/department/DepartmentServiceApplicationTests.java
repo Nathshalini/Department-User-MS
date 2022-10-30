7479224be2be3f401b4d11fcd02facba33994fdd
Nathshalini <nathshalini98@gmail.com>
2022-10-30 16:18:47 +0530

    department ms first commit

---
+++ src/test/java/com/shalini/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shalini.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
